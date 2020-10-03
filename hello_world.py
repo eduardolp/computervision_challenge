677c3f0314bdf5f658b9db52805712d1d958dbf8
Eduardo <elacerdap@gmail.com>
2020-10-02 23:49:17 -0300

    Routine commit

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,22 @@
 import cv2 as cv
+import numpy as np
 import sys
 
 cv.samples.addSamplesDataSearchPath('/home/eduardo/opencv/samples/data')
-img = cv.imread(cv.samples.findFile("starry_night.jpg"))
+# img = cv.imread(cv.samples.findFile("starry_night.jpg"))
+img = cv.imread(cv.samples.findFile('messi5.jpg'))
 if img is None:
     sys.exit("Could not read the image.")
+
+# Displays the images and waits until the user presses a key to close it. If the
+# key pressed is 's', saves the image
 cv.imshow("Display window", img)
 k = cv.waitKey(0)
 if k == ord("s"):
     cv.imwrite("starry_night.png", img)
+
+px = img[100,100]
+# print(px)
+
+blue = img[100,100,0]
+print(blue)
