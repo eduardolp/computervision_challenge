3d3f11670e2a63e895180291e3c43db7e307f971
Eduardo L. Pereira <elacerdap@gmail.com>
2020-10-03 15:19:35 -0300

    Add image blur

--- real_coins.py
+++ real_coins.py
@@ -10,10 +10,22 @@ if img is None:
     sys.exit("Could not read the image.")
 
 # Displays the images and waits until the user presses a key to close it
-cv.imshow("Display window", img)
+cv.imshow("Original image", img)
 k = cv.waitKey(0)
 
-# Convert into grayscale
+# Convert into grayscale and display it
 img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow("Display window", img_gray)
+cv.imshow("Grayscale image", img_gray)
+k = cv.waitKey(0)
+
+# Blur image and display it
+# 2D Convolution
+# kernel = np.ones((20,20), np.float32)/20**2
+# img_blurred_conv = cv.filter2D(img_gray, -1, kernel)
+# cv.imshow("Blurred image - 2D Convolution", img_blurred)
+# k = cv.waitKey(0)
+
+# Averaging
+img_blurred = cv.blur(img_gray, (20,20))
+cv.imshow("Blurred image - Averaging 20x20", img_blurred)
 k = cv.waitKey(0)
