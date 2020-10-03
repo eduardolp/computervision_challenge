d69e76cb71ea0dcd5302e3a4af2032e1f81f725c
Eduardo L. Pereira <elacerdap@gmail.com>
2020-10-03 17:12:48 -0300

    Add binary threshold to the image

--- dolar_coins.py
+++ dolar_coins.py
@@ -10,25 +10,19 @@ if img is None:
     sys.exit("Could not read the image.")
 
 # Displays the images and waits until the user presses a key to close it
-cv.imshow("Original image", img)
-k = cv.waitKey(0)
+# cv.imshow("Original image", img)
+# k = cv.waitKey(0)
 
-# # Convert into grayscale and display it
-# img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# Convert into grayscale and display it
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 # cv.imshow("Grayscale image", img_gray)
 # k = cv.waitKey(0)
-#
-# # Blur image and display it
-# # 2D Convolution
-# # kernel = np.ones((20,20), np.float32)/20**2
-# # img_blurred_conv = cv.filter2D(img_gray, -1, kernel)
-# # cv.imshow("Blurred image - 2D Convolution", img_blurred)
-# # k = cv.waitKey(0)
-#
-# # Averaging
-# img_blurred = cv.blur(img_gray, (20,20))
-# cv.imshow("Blurred image - Averaging 20x20", img_blurred)
-# k = cv.waitKey(0)
+
+# Binary threshold conversion
+ret, img_bin = cv.threshold(img_gray,35,255,cv.THRESH_BINARY_INV)
+cv.imshow("Binary Threshold", img_bin)
+k = cv.waitKey(0)
+
 #
 # # Circle detection
 # # detect circles in the image
