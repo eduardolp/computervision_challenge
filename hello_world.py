b867c2f1f395b92fd2cd34f556f7c958f0d9f9e6
Eduardo <elacerdap@gmail.com>
2020-10-02 23:38:36 -0300

    Add hello world script

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+import sys
+
+cv.samples.addSamplesDataSearchPath('/home/eduardo/opencv/samples/data')
+img = cv.imread(cv.samples.findFile("starry_night.jpg"))
+if img is None:
+    sys.exit("Could not read the image.")
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
+if k == ord("s"):
+    cv.imwrite("starry_night.png", img)
